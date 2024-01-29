5c09435562286d54b25a7a0914ef25cf32d69884
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2024-01-29 15:28:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ while True:
         if time.time()-start > 5:
             print("timer started")
         start = time.time()
-    if pyautogui.locateOnScreen('end.png', confidence = 0.7):
+        enable = True
+    if pyautogui.locateOnScreen('end.png', confidence = 0.7) and enable == True:
         end = int(abs(start - time.time())-10)
         if end - (int(end/60))*60 >= 10:
             seconds = str(end - (int(end/60))*60)
@@ -15,4 +16,4 @@ while True:
         with open("times.txt", "a") as file:
             file.write(str(int(end/60)) + ":" + seconds + "\n")
         print("gg, the time is: " + str(int(end / 60)) + ":" + str(end - (int(end / 60)) * 60))
-        time.sleep(5)
+        enable = False
